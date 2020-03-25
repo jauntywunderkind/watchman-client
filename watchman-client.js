9bd82cdda050bf50255d1ea294ffa114758de8ef
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-03-25 19:28:08 -0400

    stop trying to extend EventReader
    probably dropping this, it doesn't quite fit

--- watchman-client.js
+++ watchman-client.js
@@ -1,6 +1,6 @@
 "use module"
 import FbWatchman from "fb-watchman"
-import EventReader from "async-iter-event-reader/event-reader.js"
+//import EventReader from "async-iter-event-reader/event-reader.js"
 import get from "voodoo-opt/get.js"
 import gets from "voodoo-opt/get.js"
 import { _command } from "./_command.js"
@@ -8,7 +8,10 @@ import { _promisify} from "./_promisify.js"
 
 const FbClient= FbWatchman.Client 
 
-const capabilityCheck= _promisify( FbClient.prototype.capabilityCheck, {name: "capabilityCheck"})
+const capabilityCheck= _promisify( FbClient.prototype.capabilityCheck, {
+	argCount: 1,
+	name: "capabilityCheck"
+})
 
 const
 	//watchProject= _makeCommand( "watch-project"),
@@ -18,7 +21,7 @@ const
 
 export let WatchmanClient= function( opt){
 	FbClient.call( this, opt)
-	EventReader.call( this, opt)
+	//EventReader.call( this, opt)
 	this.project= {}
 	return this
 }
@@ -44,7 +47,7 @@ WatchmanClient.prototype= Object.create( FbClient.prototype, {
 	//subscribe: {
 	//	value: subscribe
 	//},
-	...Object.getOwnPropertyDescriptors( EventReader.prototype)
+	//...Object.getOwnPropertyDescriptors( EventReader.prototype)
 })
 
 function watchProject( path){
