795af07a44bffcaea424aee0e22c6a1850537cf3
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-03-25 19:26:48 -0400

    add _mint.only for making tests

--- .test/_mint.js
+++ .test/_mint.js
@@ -2,10 +2,21 @@ import tape from "tape"
 
 /** produce a new test instance runner */
 export function _mint( key, desc, fn){
+	let _tape= this&& this.tape|| tape
 	return ({[ key]: function(){
-		tape( desc, fn)
+		_tape( desc, async function testErrorCatcherWrapper( t){
+			try{
+				await fn( t)
+			}catch(err){
+				console.error("error", err)
+			}
+		})
 	}})[ key]
 }
 export const mint= _mint
 export default _mint
 
+export const only= function only( key, desc, fn){
+	return _mint.call({ tape: tape.only}, key, desc, fn)
+}
+_mint.only= only
