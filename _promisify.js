3f29d48ee448e9f42b6d13e9644592afcfa710bd
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-03-25 19:27:38 -0400

    _promisify now pads out it's args

--- _promisify.js
+++ _promisify.js
@@ -1,20 +1,30 @@
 /** turn a function `fn` into a function with accepts an optional callback, and is async if there is no callback */
-export function _promisify( fn, { argCount= 1, name}= { name: fn.name}){
+export function _promisify( fn, { argCount= 0, name}= { name: fn.name}){
 	// create & de-reference our function within an object, such that it retains `fn.name`
 	return ({[ name]: function( ...args){
+		// a callback way provided - run as normal
 		if( args.length> argCount){
 			return fn.call( this, ...args)
-		}else{
-			return new Promise(( resolve, reject)=>{
-				fn.call( this, ...args, function( err, ok){
-					if( err){
-						reject( err)
-					}else{
-						resolve( ok)
-					}
-				})
-			})
 		}
+
+		// pad args
+		const underfill= argCount- args.length
+		if( underfill> 0){
+			args.push(...new Array(underfill))
+		}
+
+		// run
+		return new Promise(( resolve, reject)=>{
+			console.log("begin", this, args, fn)
+			fn.call( this, ...args, function( err, ok){
+				console.log("hi")
+				if( err){
+					reject( err)
+				}else{
+					resolve( ok)
+				}
+			})
+		})
 	}})[ name]
 }
 export default _promisify
