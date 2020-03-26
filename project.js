66f41a687509a82cf3f49c9c33900c6669030b0d
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-03-26 00:05:37 -0400

    project#clock, towards subscribe. allow project#client.
    clock is a common feature on subscription. had resisted
    having a two way relationship betweeen project & client
    but this is going to be easier.

--- project.js
+++ project.js
@@ -4,36 +4,48 @@ import CanonicalStringify from "canonical-json/index2.js"
 import { WatchSubscribe} from "./subscribe.js"
 import _command from "./_command.js"
 
-const _watchProject= _command( "watch-project")
+const
+	_clock= _command( "clock"),
+	_watchProject= _command( "watch-project")
 
 export function WatchProject( path, client){
 	this.path= path
-	this.subscriptions= {}
+	this.subscription= {}
+	this.client= client
 	this._rebindWatch( client)
 	return this
 }
 export default WatchProject
 
-WatchProject.prototype.then= function( ok, bad){
-	return this._watch.then( ok, bad)
-}
-
-WatchProject.prototype._rebindWatch= function( client){
-	this._watch= client? _watchProject.call( client, this.path): Promise.reject("No Watch")
-	// TODO: do we want to re-build all subscriptions we have? maybe
-}
-
-WatchProject.prototype.subscribe= async function( name, opt, client){
-	const optText= CanonicalStringify( opt)
-	let sub= this.subscriptions[ subText]
-	if( sub){
-		return sub
+WatchProject.prototype._rebindWatch= function(){
+	if( !this.client){
+		this._watch= Promise.reject("No Watch")
+	}else{
+		this._watch= _watchProject.call( this.client, this.path)
 	}
+	// TODO: do we want to re-build all subscription we have? maybe
+}
+
+WatchProject.prototype.clock= async function(){
+	const _watch= await this._watch
+	return _clock.call( this.client, _watch.watch)
+}
+
+WatchProject.prototype.subscribe= async function( name, sub, client){
+	const subText= CanonicalStringify( sub)
+	let s= this.subscription[ subText]
+	if( s){
+		return s
+	}
+
 	const
-		_watch= await this._watch,
-		relativePath = _watch.relative_path
+		[ _watch, clock]= await Promise.all([
+			this._watch,
+			sub.clock=== true? client.clock(): null
+		]),
+		relative_path = _watch.relative_path
 	opt= Object.assign({}, opt, { relative_path})
 	sub= new Subscribe( _watch.watch, name, opt, client)
-	this.subscriptions[ subText]= sub
+	this.subscription[ subText]= sub
 	return sub[ Symbol.asyncIterator]()
 }
