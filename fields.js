7809d07c4336e6b8132001dc43798f8bfc7c7be3
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-03-26 12:08:18 -0400

    fields.js file

---
+++ fields.js
@@ -0,0 +1,13 @@
+export const Fields= {
+	name: "name",
+	size: "size",
+	mtime_ms: "mtime_ms",
+	exists: "exists",
+	type: "type"
+}
+export {
+	Fields as default,
+	Fields as fields
+}
+
+export const AllFields= Object.keys( Fields)
